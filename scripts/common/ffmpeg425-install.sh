3dc5e65331d2c04b1588220325f10b73670123c8
jario-jin <jariof@foxmail.com>
2023-06-20 09:28:23 +0000

    Fallback scripts/common/ffmpeg425-install.sh.
    Signed-off-by: jario-jin <jariof@foxmail.com>

--- scripts/common/ffmpeg425-install.sh
+++ scripts/common/ffmpeg425-install.sh
@@ -5,8 +5,6 @@ sudo apt install -y \
 build-essential yasm cmake libtool libc6 libc6-dev unzip wget libfmt-dev \
 libnuma1 libnuma-dev libx264-dev libx265-dev libfaac-dev libssl-dev
 
-sudo apt install -y ffmpeg
-
 root_dir=${HOME}"/SpireCV"
 if [ ! -d ${root_dir} ]; then
   echo -e "\033[32m[INFO]: ${root_dir} not exist, creating it ... \033[0m"
@@ -14,37 +12,37 @@ if [ ! -d ${root_dir} ]; then
 fi
 cd ${root_dir}
 
-#git clone https://gitee.com/jario-jin/nv-codec-headers.git
-#cd nv-codec-headers
-#git checkout n11.1.5.0
-#sudo make install
-#cd ..
+git clone https://gitee.com/jario-jin/nv-codec-headers.git
+cd nv-codec-headers
+git checkout n11.1.5.0
+sudo make install
+cd ..
 
-#wget https://ffmpeg.org/releases/ffmpeg-4.2.5.tar.bz2
-#tar -xjf ffmpeg-4.2.5.tar.bz2
-#cd ffmpeg-4.2.5
-#export PATH=$PATH:/usr/local/cuda/bin
-#sed -i 's#_30#_75#' configure; sed -i 's#_30#_75#' configure
-#./configure \
-#--enable-nonfree \
-#--enable-gpl \
-#--enable-shared \
-#--enable-ffmpeg \
-#--enable-ffplay \
-#--enable-ffprobe \
-#--enable-libx264 \
-#--enable-libx265 \
-#--enable-cuda-nvcc \
-#--enable-nvenc \
-#--enable-cuda \
-#--enable-cuvid \
-#--enable-libnpp \
-#--extra-libs="-lpthread -lm" \
-#--extra-cflags=-I/usr/local/cuda/include \
-#--extra-ldflags=-L/usr/local/cuda/lib64 
-#make -j8
-#sudo make install
-#cd ..
+wget https://ffmpeg.org/releases/ffmpeg-4.2.5.tar.bz2
+tar -xjf ffmpeg-4.2.5.tar.bz2
+cd ffmpeg-4.2.5
+export PATH=$PATH:/usr/local/cuda/bin
+sed -i 's#_30#_75#' configure; sed -i 's#_30#_75#' configure
+./configure \
+--enable-nonfree \
+--enable-gpl \
+--enable-shared \
+--enable-ffmpeg \
+--enable-ffplay \
+--enable-ffprobe \
+--enable-libx264 \
+--enable-libx265 \
+--enable-cuda-nvcc \
+--enable-nvenc \
+--enable-cuda \
+--enable-cuvid \
+--enable-libnpp \
+--extra-libs="-lpthread -lm" \
+--extra-cflags=-I/usr/local/cuda/include \
+--extra-ldflags=-L/usr/local/cuda/lib64 
+make -j8
+sudo make install
+cd ..
 
 git clone https://gitee.com/jario-jin/ZLMediaKit.git
 cd ZLMediaKit
