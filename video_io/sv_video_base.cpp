f5056d69e6134309a1fce00e5a158f8c174c407b
jario-jin <jariof@foxmail.com>
2023-07-05 17:33:25 +0800

    sync a-params & del unused video_base codes

--- video_io/sv_video_base.cpp
+++ video_io/sv_video_base.cpp
@@ -1078,61 +1078,7 @@ bool CameraBase::isRunning()
 
 void CameraBase::openImpl()
 {
-  if (this->_type == CameraType::WEBCAM)
-  {
-    this->_cap.open(this->_camera_id);
-    if (this->_width > 0 && this->_height > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_FRAME_WIDTH, this->_width);
-      this->_cap.set(cv::CAP_PROP_FRAME_HEIGHT, this->_height);
-    }
-    if (this->_fps > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_FPS, this->_fps);
-    }
-    if (this->_brightness > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_BRIGHTNESS, this->_brightness);
-    }
-    if (this->_contrast > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_CONTRAST, this->_contrast);
-    }
-    if (this->_saturation > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_SATURATION, this->_saturation);
-    }
-    if (this->_hue > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_HUE, this->_hue);
-    }
-    if (this->_exposure > 0)
-    {
-      this->_cap.set(cv::CAP_PROP_EXPOSURE, this->_exposure);
-    }
-  }
-  else if (this->_type == CameraType::G1)
-  {
-    char pipe[512];
-    if (this->_width <= 0 || this->_height <= 0)
-    {
-      this->_width = 1280;
-      this->_height = 720;
-    }
-    if (this->_port <= 0)
-    {
-      this->_port = 554;
-    }
-    if (this->_fps <= 0)
-    {
-      this->_fps = 30;
-    }
-    // sprintf(pipe, "rtsp://%s:%d/H264?W=%d&H=%d&BR=10000000&FPS=%d", this->_ip.c_str(), this->_port, this->_width, this->_height, this->_fps);
-    sprintf(pipe, "rtspsrc location=rtsp://%s:%d/H264?W=%d&H=%d&FPS=%d&BR=4000000 latency=100 ! application/x-rtp,media=video ! rtph264depay ! parsebin ! nvv4l2decoder enable-max-performancegst=1 ! nvvidconv ! video/x-raw,format=(string)BGRx ! videoconvert ! appsink sync=false", this->_ip.c_str(), this->_port, this->_width, this->_height, this->_fps);
-    // std::cout << pipe << std::endl;
-    // this->_cap.open(pipe);  // cv::CAP_GSTREAMER
-    this->_cap.open(pipe, cv::CAP_GSTREAMER);
-  }
+
 }
 void CameraBase::open(CameraType type, int id)
 {
