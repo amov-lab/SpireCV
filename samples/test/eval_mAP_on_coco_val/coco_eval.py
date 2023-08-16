986772c5b0b393640715706f72809a42dee4a062
lxm <1367240116@qq.com>
2023-08-16 20:50:47 +0800

    COCO Model Eval

---
+++ samples/test/eval_mAP_on_coco_val/coco_eval.py
@@ -0,0 +1,37 @@
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+import os
+import json
+
+if __name__ == '__main__':
+    path = os.path.abspath(os.path.join(os.getcwd(),"../../.."))
+    pred_json = 'pd_coco.json'
+    anno_json = path + '/val2017/instances_val2017.json'
+    
+    # use COCO API to load forecast results and annotations
+    cocoGt = COCO(anno_json)
+    with open(pred_json,'r') as file:
+      data = json.load(file)
+    
+    # align anno_json with pred_json category_id
+    gtCatDicts = {}
+    for anns in range(len(cocoGt.getCatIds())):
+      gtCatDicts[anns] = cocoGt.getCatIds()[anns]
+
+    pdCatIds=list(set([d['category_id'] for d in data]))
+
+    if not set(pdCatIds).issubset(set(cocoGt.getCatIds())):
+        for ins in data:
+           temp = int(gtCatDicts[ins['category_id']])
+           ins['category_id'] = temp
+
+    # load prediction results
+    cocoDt = cocoGt.loadRes(data)
+ 
+    # create COCO eval object
+    cocoEval = COCOeval(cocoGt, cocoDt,'bbox')
+
+    # assessment
+    cocoEval.evaluate()
+    cocoEval.accumulate()
+    cocoEval.summarize()
---
+++ samples/test/eval_mAP_on_coco_val/pd2cocojson.py
@@ -0,0 +1,53 @@
+import datetime
+import json
+import os
+import cv2
+
+# revert prediction results to coco_json 
+path = os.path.abspath(os.path.join(os.getcwd(),"../../.."))
+
+# all files dir
+images_path = path+'/val2017/val2017'
+preds_path = path+'/val2017/preds'
+coco_json_save ='pd_coco.json'
+
+# config coco_json
+coco_json = []
+
+# load images dir
+images = os.listdir(images_path)
+for image in images:
+    # get image name
+    image_name, image_suffix = os.path.splitext(image)
+    # get image W and H
+    image_path = images_path + '/' + image
+    img = cv2.imread(image_path)
+    height, width, _ = img.shape
+
+    # read pred's txt
+    pred_path = preds_path + '/' + image_name + '.txt'
+    if not os.path.exists(pred_path):
+      continue
+    with open(pred_path, 'r') as f:
+      preds = f.readlines()
+      preds = [l.strip() for l in preds]
+      for j,pred in enumerate(preds):
+        pred = pred.split(' ')
+        category_id = int(pred[0])
+        x = float(pred[1]) * width
+        y = float(pred[2]) * height
+        w = float(pred[3]) * width
+        h = float(pred[4]) * height
+        xmin = x - w / 2
+        ymin = y - h / 2
+        
+        coco_json.append({
+         'image_id': int(image_name),
+         'category_id': int(category_id),
+         'bbox': [xmin, ymin, w, h],
+         'score': float(pred[5])})
+
+# save json
+with open(os.path.join(coco_json_save), 'w') as f:
+    json.dump(coco_json, f, indent=2)
+print(len(coco_json), 'Done!')
